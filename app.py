0037438ae8cb9f47d43485ca3addab145229598a
Timer <konkin_anton@list.ru>
2020-08-13 13:20:20 +0400

    Initial commit, sort of working prototype.

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    print(request.form['text'])
+    return redirect('/create')
+
+@app.route('/archive')
+def archive():
+    return render_template('archive.html')
+
+@app.route('/article/<id_>')
+def article(id_=-1):
+    return render_template('article.html')
