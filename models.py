98fe064bbd319a806c2a58524374c1eea1a252a8
Timer <konkin_anton@list.ru>
2020-08-15 22:13:24 +0400

    Создал модели пользователя и комментария.

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import *
+from sqlalchemy.orm import relationship
 from db import Base
 
 
@@ -8,6 +9,31 @@ class Article(Base):
     title = Column(String)
     text = Column(String)
     created = Column(DateTime)
+    author_id = Column(Integer, ForeignKey('users.id'))
+
+    author = relationship('User', back_populates='articles')
+    comments = relationship('Comment', back_populates='article')
 
     def __repr__(self):
         return f'<Article "{self.title}">'
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+    is_author = Column(Boolean)
+    is_admin = Column(Boolean)
+
+    articles = relationship('Article', order_by=Article.id, back_populates='author')
+    comments = relationship('Comment', order_by=Article.id, back_populates='user')
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    article_id = Column(Integer, ForeignKey('articles.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    text = Column(String)
+
+    article = relationship('Article', back_populates='comments')
+    user = relationship('User', back_populates='comments')
